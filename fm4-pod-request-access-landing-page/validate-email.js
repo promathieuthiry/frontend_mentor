f3c1abbb94f5f4a10c41d151810bde24a7be058d
Mathieu <promathieuthiry@gmail.com>
2021-08-12 15:38:42 -0400

    landing page setup

---
+++ fm4-pod-request-access-landing-page/validate-email.js
@@ -0,0 +1,29 @@
+function ValidateEmail(inputText, mobile) {
+    console.log(inputText)
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.match(mailformat)) {
+        console.log("match")
+        return true;
+    }
+    else {
+        if (mobile) {
+            console.log("isMobile", true)
+            document.getElementById("email-error-message-mobile").style.opacity = "1";
+            return false;
+        } else {
+            document.getElementById("email-error-message").style.opacity = "1";
+            return false;
+        }
+
+    }
+}
+
+function Erase(mobile) {
+    if (mobile) {
+        document.getElementsByClassName('input-email-mobile')[0].value = ""
+        return document.getElementById("email-error-message-mobile").style.opacity = "0";
+    } else {
+        document.getElementsByClassName('input-email')[0].value = ""
+        return document.getElementById("email-error-message").style.opacity = "0";
+    }
+}
