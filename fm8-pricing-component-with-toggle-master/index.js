ad517fdf6683216fed5e4ec52c9e31eb7c7b13ff
Mathieu <promathieuthiry@gmail.com>
2021-08-24 11:07:59 -0400

    countdown terminated

--- fm8-pricing-component-with-toggle-master/index.js
+++ fm8-pricing-component-with-toggle-master/index.js
@@ -23,3 +23,41 @@ function showHide() {
         }
     }
 }
+
+function showAnnually() {
+    // Get the checkbox
+    var checkBox = document.getElementById("toggle");
+    let annually = document.getElementsByClassName("show-annually");
+    let monthly = document.getElementsByClassName("show-montly");
+    let whiteball = document.getElementsByClassName("checkbox-whiteball");
+
+    if (checkBox.checked == false) {
+        whiteball[0].style.transform = "translateX(-24px)"
+        for (let i = 0; i < annually.length; i++) {
+            annually[i].style.display = "block"
+        }
+        for (let i = 0; i < monthly.length; i++) {
+            monthly[i].style.display = "none"
+        }
+        checkBox.checked = true
+    }
+}
+
+function showMonthly() {
+    // Get the checkbox
+    var checkBox = document.getElementById("toggle");
+    let annually = document.getElementsByClassName("show-annually");
+    let monthly = document.getElementsByClassName("show-montly");
+    let whiteball = document.getElementsByClassName("checkbox-whiteball");
+
+    if (checkBox.checked == true) {
+        whiteball[0].style.transform = "translateX(0px)"
+        for (let i = 0; i < annually.length; i++) {
+            annually[i].style.display = "none"
+        }
+        for (let i = 0; i < monthly.length; i++) {
+            monthly[i].style.display = "block"
+        }
+        checkBox.checked = false
+    }
+}
---
+++ fm9-launch-countdown-timer-main/index.js
@@ -0,0 +1,47 @@
+let future = new Date();
+future.setDate(future.getDate() + 14)
+const futur2 = future.getTime()
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    const currentTime = new Date().getTime();
+    // const countDownDate =  currentTime.setDate(currentTime.getDate() + 14);
+
+
+    // Find the distance between now and the count down date
+    var distance = futur2 - currentTime;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    const daysText = document.getElementsByClassName("days");
+    const hoursText = document.getElementsByClassName("hours");
+    const minutesText = document.getElementsByClassName("minutes");
+    const secondsText = document.getElementsByClassName("seconds");
+
+    for (let i = 0; i < daysText.length; i++) {
+        daysText[i].innerHTML = days
+    }
+    for (let i = 0; i < hoursText.length; i++) {
+        hoursText[i].innerHTML = hours
+    }
+    for (let i = 0; i < minutesText.length; i++) {
+        minutesText[i].innerHTML = minutes
+    }
+    for (let i = 0; i < secondsText.length; i++) {
+        secondsText[i].innerHTML = seconds
+    }
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        alert("E")
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
