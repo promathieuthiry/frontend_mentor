854e6caed68a4f8004df4d01d3144248e92a0d98
Mathieu <promathieuthiry@gmail.com>
2021-10-21 09:50:27 -0400

    init

---
+++ fm12-todo-app-main/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Todo All Active Completed Clear Completed Drag and drop to reorder list
+    </div>
+  );
+}
+
+export default App;
